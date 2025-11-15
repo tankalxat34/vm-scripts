ffd951b77ad497d76e64e7b6ad2d82649df10cd3
tankalxat34 <tankalxat34@gmail.com>
2025-11-15 18:48:34 +0000

    почти финальная версия

--- install_postgresql.sh
+++ install_postgresql.sh
@@ -31,31 +31,39 @@ echo
 echo "##################################################"
 echo "Запускаем установку PostgreSQL"
 
+touch /scripts/install_postgresql.log
+
 echo " 1 - Конфигурация дерева установки с параметрами сборки"
+cd $path_to_sources
+
+echo "  Очищаю предыдущую сборку"
+date && time make distclean >> /scripts/install_postgresql.log 2>&1 && date
+
 ./configure \
 	--prefix=$CONF_PREFIX \
 	--with-block-compression \
 	--with-path-checksum \
-	"${OPTIONS[@]}" > install_postgresql.log 2>&1
+	"${OPTIONS[@]}" >> /scripts/install_postgresql.log 2>&1
 echo "  Успешно!"
 
 echo
 echo " 2 - Запускам сборку"
 if [ $? -eq 0 ]; then
-	make && \
-	echo " 3 - Тестирование сборки" && \
-	make check
+	echo "  Начинаю новую сборку"
+	date && time make && date >> /scripts/install_postgresql.log  2>&1 && date && echo "  Сборка окончена"
+	echo "  Тестирую сборку"
+	date && time make check && date >> /scripts/install_postgresql.log  2>&1 && date
 else
 	echo "Конфигурация с ошибками, продолжение невозможно"
 	exit 2
 fi
 
 echo " 4 - Устанавливаем сборку"
-make install && echo "Сборка успешно установлена!"
+date && time make install >> /scripts/install_postgresql.log  2>&1 && date && echo "Сборка успешно установлена!"
 
 echo "##################################################"
 echo " 5 - Меняем владельца в каталоге установки"
-chown -R postgres:postgres $CONF_PREFIX
+chown -R postgres:postgres "${CONF_PREFIX}"
 
 echo
 echo " 6 - Устанавливаем переменные окружения"
@@ -81,14 +89,14 @@ curdir="/data/pg_data/"
 if [ -d "${curdir}" ]; then
 	echo "${curdir} существует"
 else
-        mkdir "${curdir}" && chown -R postgres:postgres $curdir && "  ${curdir} создана."
+        mkdir "${curdir}" && chown -R postgres:postgres "${curdir}" && echo "  ${curdir} создана."
 fi
 
 curdir="/log/pg_log"
 if [ -d "${curdir}" ]; then
         echo "${curdir} существует"
 else
-        mkdir "${curdir}" && chown -R postgres:postgres $curdir && "  ${curdir} создана."
+        mkdir "${curdir}" && chown -R postgres:postgres "${curdir}" && echo "  ${curdir} создана."
 fi
 
 
@@ -96,23 +104,34 @@ curdir="/wal/pg_wal"
 if [ -d "${curdir}" ]; then
         echo "${curdir} существует"
 else
-        mkdir "${curdir}" && chown -R postgres:postgres $curdir && "  ${curdir} создана."
+        mkdir "${curdir}" && chown -R postgres:postgres "${curdir}" && echo "  ${curdir} создана."
 fi
 
 
 echo
 echo " 8 - Инициализируем кластер СУБД в /data/pg_data"
-sudo -u postgres $CONF_PREFIX/bin/initdb -k -D /data/pg_data --waldir=/wal/pg_wal && echo " кластер инициализирован"
+sudo -u postgres "${CONF_PREFIX}/bin/initdb" -k \
+	--locale="ru_RU.UTF-8" \
+	--encoding="UTF8" \
+	--timezone="Europe/Moscow" \
+	-D /data/pg_data \
+	--waldir=/wal/pg_wal \
+	&& echo " кластер инициализирован"
 
 echo
 echo " 9 - Вносим базовые настройки в postgresql.conf"
 
 echo
-echo " 9 - Запускаем кластер СУБД. Лог пишем в /log/pg_log"
-sudo -u postgres $CONF_PREFIX/bin/pg_ctl -D /data/pg_data -l /log/pg_log && echo " кластер запущен"
+echo " 10 - Запускаем кластер СУБД. Лог пишем в /log/pg_log"
+sudo -u postgres "${CONF_PREFIX}/bin/pg_ctl" -D /data/pg_data -l /log/pg_log && echo " кластер запущен"
 
 echo
-echo " 10 - Проверка соединения"
-psql -U postgres -d postgres -c "SELECT now();"
-psql -U postgres -d postgres -c "SELECT pg_version();"
+echo " 11 - Проверка соединения"
+sudo -u postgres "${CONF_PREFIX}/bin/psql" -c "SELECT now();"
+sudo -u postgres "${CONF_PREFIX}/bin/psql" -c "SELECT pg_version();"
 
+echo
+echo "##################################################"
+echo "СУБД PostgreSQL успешно установлена. Для "
+echo "применения переменных окружения и путей к"
+echo "исполняемым файлам необходимо перезайти на сервер."
---
+++ remove_postgresql.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm /etc/profile.d/pgsql.sh
+rm -rf /data/pg_data /log/pg_log /wal/pg_wal
+rm -rf /usr/local/pgsql 
